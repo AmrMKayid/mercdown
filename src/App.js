1e0384ed767044fdc38694b2fc2d657d0f765461
Amr M. Kayid <amrkayid2027@gmail.com>
2018-08-14 14:59:47 +0200

    Implement simple editor with codemirror

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 import SplitPane from 'react-split-pane';
+import Editor from './editor';
 
 
 class App extends Component {
+
+    constructor(props) {
+        super();
+
+        this.state = {
+            markdownSrc: "# Hello World",
+        }
+    }
+
+
     render() {
         return (
             <div className="App">
                 <SplitPane split="vertical" defaultSize="50%">
-                    <div className={"editor-pane"}></div>
+                    <div className={"editor-pane"}>
+                        <Editor className="editor" value={this.state.markdownSrc}/>
+                    </div>
 
                     <div className={"view-pane"}></div>
                 </SplitPane>
